6753a58ede9a2a760ecb5fcafa15f3b4422734af
Zhujin Liang <alfredtofu@gmail.com>
2016-07-29 10:20:43 +0800

    first commit

---
+++ lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.15")
+SET(OpenBLAS_INCLUDE_DIRS /home/liangzhujin/env/OpenBLAS//include)
+SET(OpenBLAS_LIBRARIES /home/liangzhujin/env/OpenBLAS//lib/libopenblas.so)
---
+++ remove_write_permission.sh
@@ -0,0 +1,1 @@
+find . -type f | grep -v .git | grep -v README.md | xargs chmod -w
